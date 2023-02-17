f8a33d215bc61cce3942aba4721f1e18f28e4185
Galantixa <nugrahafajarrr@gmail.com>
2023-02-17 21:43:46 +0700

    #

--- src/client.ts
+++ src/client.ts
@@ -1,4 +1,4 @@
-// TODO 
+// TODO
 // query a specific user by id
 // create a new user
 // get all users
@@ -7,18 +7,26 @@ import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
 import { AppRouter } from "./server";
 
 const client = createTRPCProxyClient<AppRouter>({
-    links: [
-        httpBatchLink({
-            url: "http://localhost:3000/trpc",
-        })
-    ]
+  links: [
+    httpBatchLink({
+      url: "http://localhost:3000/trpc",
+      headers: {
+        authorization: "1",
+      },
+    }),
+  ],
 });
 
 async function main() {
-    await client.getUserById.query({id: 1}).then( user => console.log("get user reached", user))
-    client.createUsers.mutate({name: "Jane"}).then ((user) => console.log("create user reached", user))
-    client.listUser.query().then( (users) => console.log("list user reached", users))
+  await client.getUserById
+    .query({ id: 1 })
+    .then((user) => console.log("get user reached", user));
+  client.createUsers
+    .mutate({ name: "Jane" })
+    .then((user) => console.log("create user reached", user));
+  client.listUser
+    .query()
+    .then((users) => console.log("list user reached", users));
 }
-
 
 main();
--- src/server.ts
+++ src/server.ts
@@ -1,52 +1,82 @@
-// TODO 
+// TODO
 // get a list of users
 // get a user by id
 // create a user
 
-import { initTRPC } from "@trpc/server";
+import { inferAsyncReturnType, initTRPC, TRPCError } from "@trpc/server";
 import { z } from "zod";
 import express from "express";
 import * as trpcExpress from "@trpc/server/adapters/express";
+import { CreateExpressContextOptions } from "@trpc/server/adapters/express";
 
 type User = {
-    id : number;
-    name: string;
+  id: number;
+  name: string;
 };
 
 const userList: User[] = [
-    {
-        id: 1,
-        name: "Jhon",
-    },
+  {
+    id: 1,
+    name: "Jhon",
+  },
 ];
 
-const trpc = initTRPC.create();
+const createContext = (opts: CreateExpressContextOptions) => {
+  const userId = opts.req.headers.authorization;
+
+  if (!userId) {
+    throw new TRPCError({ code: "UNAUTHORIZED", message: "No USER id" });
+  }
+  const user = userList.find((user) => user.id === +userId);
+
+  if (!user) {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+      message: "no matching user was found",
+    });
+  }
+  return {
+    ...opts,
+    user,
+  };
+};
+
+const trpc = initTRPC
+  .context<inferAsyncReturnType<typeof createContext>>()
+  .create();
+
 const appRouter = trpc.router({
-    listUser: trpc.procedure.query(() => userList),
-    getUserById: trpc.procedure.input(z.object({id: z.number()})).query( ({input}) => {
-        return userList.find (user => user.id === input.id);
+  listUser: trpc.procedure.query(({ ctx }) => {
+    console.log("Requesting user: ", ctx.user.name);
+    return userList;
+  }),
+  getUserById: trpc.procedure
+    .input(z.object({ id: z.number() }))
+    .query(({ input }) => {
+      return userList.find((user) => user.id === input.id);
     }),
-    createUsers : trpc.procedure.input(z.object({name: z.string()})).mutation(({input}) => {
-        const newUser : User = {
-            name: input.name,
-            id: userList[userList.length -1].id+ 1,
-        }
-        userList.push(newUser);
-        return newUser;
+  createUsers: trpc.procedure
+    .input(z.object({ name: z.string() }))
+    .mutation(({ input }) => {
+      const newUser: User = {
+        name: input.name,
+        id: userList[userList.length - 1].id + 1,
+      };
+      userList.push(newUser);
+      return newUser;
     }),
 });
-
 
 const app = express();
 
 export type AppRouter = typeof appRouter;
 
 app.use(
-    "/trpc",
-    trpcExpress.createExpressMiddleware ({
-        router: appRouter,
-    })
+  "/trpc",
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext,
+  })
 );
-
 
 app.listen(3000, () => console.log(`App is running on port ${3000}`));
